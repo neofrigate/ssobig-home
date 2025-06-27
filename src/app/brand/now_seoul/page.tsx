ea0db8d5a26c36b53a3adde49c94a6a5fb493c99
as-surin <surin@ssobig.com>
2025-06-27 12:15:54 +0900

    픽셀코드 수정

--- src/app/brand/now_seoul/page.tsx
+++ src/app/brand/now_seoul/page.tsx
@@ -3,6 +3,7 @@ import Card, { CardProps } from "../../../components/Card";
 import ProfileSlider, {
   ProfileCardProps,
 } from "../../../components/ProfileSlider";
+import Script from "next/script";
 
 export const metadata = {
   title: "Ssobig-N.O.W.seoul 나우서울",
@@ -77,6 +78,26 @@ export default function NowSeoulPage() {
 
   return (
     <>
+      {/* Meta Pixel Code */}
+      <Script
+        id="facebook-pixel-now-seoul"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1409643070210008');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      {/* End Meta Pixel Code */}
       <div className="min-h-screen text-white font-sans relative flex flex-col items-center justify-start pb-4 px-0 selection:bg-blue-500 selection:text-white pt-[72px]">
         {/* 배경 이미지 next/image 적용 */}
         <div className="fixed inset-0 -z-10">
